87d4820b43f38477533ad56281c4c629c854cf4d
Salty Mat <mathew.zahedi@gmail.com>
2022-04-09 14:06:46 -0700

    asdfghj

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ if (fs.existsSync(countPath)) {
 const webhookClient = new WebhookClient({ url: config.webhookUrl });
 
 const job = schedule.scheduleJob({hour:6 , minute: 0}, () => {
-	webhookClient.send(`***__Good Morning__*** its **6** AM in pacific time **|** **${count}** days till spring break`);
+	webhookClient.send(`***__Good Morning__*** its **6** AM in pacific time **|** **${count}** days till summer break.`);
 	count--;
 	saveCountToFile();
 	console.log('webhook sent');
